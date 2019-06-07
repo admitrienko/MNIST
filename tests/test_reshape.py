d2547163e330bf952543e682e32acaccc7497e47
admitrienko <34189027+admitrienko@users.noreply.github.com>
2019-06-07 14:00:26 -0400

    Update test_reshape.py

--- tests/test_reshape.py
+++ tests/test_reshape.py
@@ -5,7 +5,7 @@ def test_reshape1():
     
     original_data = np.array([[1,2,3],[4,5,6],[7,8,9],[10,11,12], [13,14,15],[16,17,18],[19,20,21],[22,23,24]])
     
-    new_data = np.reshape(data, (4,2,3), order = 'F')
+    new_data = np.reshape(original_data, (4,2,3), order = 'F')
     
     assert np.array_equal(new_data[:,0], original_data[0:4])
 
