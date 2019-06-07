1da3c6c7c18324e4478c0f71fca00fa64affd0c9
admitrienko <34189027+admitrienko@users.noreply.github.com>
2019-06-07 11:30:38 -0400

    Update test_reshape.py

--- tests/test_reshape.py
+++ tests/test_reshape.py
@@ -8,12 +8,24 @@ def reshape1(data):
 
 def test_reshape1():
 
-    input1 = np.array([[1,2,3,4],[5,6,7,8]])
+    input0 = np.array([[1,2,3,4],[5,6,7,8]])
 
-    output1 = np.array([[1,2],[3,4]])
+    output0 = np.array([[1,2],[3,4]])
 
-    output2 = np.array([[5,6],[7,8]])
+    output1 = np.array([[5,6],[7,8]])
+    
+    reshaped = reshape1(input0)
+    
+    reshaped0 = reshaped[0]
+    reshaped1 = reshaped[1]
 
-    assert reshape1(input1)[0] == output1
-    assert reshape1(input1)[1] == output2
+    assert reshaped0[0] == output1[0]
+    assert reshaped0[1] == output1[1]
+    assert reshaped0[2] == output1[2]
+    assert reshaped0[3] == output1[3]
+    
+    assert reshaped1[0] == output2[0]
+    assert reshaped1[1] == output2[1]
+    assert reshaped1[2] == output2[2]
+    assert reshaped1[3] == output2[3]
     
