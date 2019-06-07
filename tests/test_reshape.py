a9e2b7db967cef3a8ee1108d31b8ccb172777bdc
admitrienko <34189027+admitrienko@users.noreply.github.com>
2019-06-07 13:30:25 -0400

    Update test_reshape.py

--- tests/test_reshape.py
+++ tests/test_reshape.py
@@ -25,7 +25,7 @@ def test_reshape1(input1, input2):
 if __name__ == "__main__":
     data = np.array([[1,2,3],[4,5,6],[7,8,9],[10,11,12], [13,14,15],[16,17,18],[19,20,21],[22,23,24]]) #8x3
     step1 = reshape1(data)
-    test_reshape1(data, step1)
+    test_reshape1(step1, data)
     
     #step2 = swap_axes1(step1)
     #test_swap_axes(data)
