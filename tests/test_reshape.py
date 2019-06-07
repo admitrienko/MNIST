37bbbfbae4c654e2eaec2cc34dad00af88e56c43
admitrienko <34189027+admitrienko@users.noreply.github.com>
2019-06-07 13:27:27 -0400

    Update test_reshape.py

--- tests/test_reshape.py
+++ tests/test_reshape.py
@@ -6,17 +6,26 @@ def reshape1(data):
 
     return data_reshaped
 
-def test_reshape1(input1):
+def test_reshape1(input1, input2):
 
-    reshaped = reshape1(input1):
+    assert np.array_equal(input1[:,0], input2[0:4])
 
+    assert np.array_equal(input1[:,1], input2[4:8])
     
-
-    assert np.array_equal(reshaped[:,0], input1[0:4])
-
-    assert np.array_equal(reshaped[:,1], input1[4:8])
+    
+#def swap_axes1():
+    
+#def test_swap_axes(input1):
+    
+    
+    
+    
+    
     
 if __name__ == "__main__":
     data = np.array([[1,2,3],[4,5,6],[7,8,9],[10,11,12], [13,14,15],[16,17,18],[19,20,21],[22,23,24]]) #8x3
-    test_reshape1(data)
+    step1 = reshape1(data)
+    test_reshape1(data, step1)
+    
+    #step2 = swap_axes1(step1)
     #test_swap_axes(data)
