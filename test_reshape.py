cceabe02121d5ce92c3f1243c5c68237d7c64ffe
admitrienko <34189027+admitrienko@users.noreply.github.com>
2019-06-07 11:24:36 -0400

    Update test_reshape.py

--- test_reshape.py
+++ test_reshape.py
@@ -1,1 +1,19 @@
 import numpy as np
+
+def reshape1(data):
+
+    data_reshaped = np.reshape(data, (2,2,2), order = 'F') 
+
+    return data_reshaped
+
+def test_reshape1():
+
+    input1 = np.array([1,2,3,4],[5,6,7,8])
+
+    output1 = np.array([1,2],[3,4])
+
+    output2 = np.array([5,6],[7,8])
+
+    assert reshape1(input1)[0] == output1
+    assert reshape1(input1)[1] == output2
+    
