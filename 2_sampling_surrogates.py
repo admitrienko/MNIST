1978a32f1ce899ddf2aa9a589671a8e9de802ab3
Anastasia Dmitrienko <34189027+admitrienko@users.noreply.github.com>
2019-07-24 15:12:43 -0400

    Update and rename sampling_surrogates_2.py to 2_sampling_surrogates.py

--- 2_sampling_surrogates.py
+++ 2_sampling_surrogates.py
@@ -1,5 +1,7 @@
 import numpy as np
 import sys
+import keras
+from keras.models import Model, Sequential
 from utils import *
 
 
@@ -11,22 +13,23 @@ def import_randtensor(pathname):
 
     return None
 
-#reduce sample size from 5420 to 2000 to make covariance computations feasible
 
-def reduce_sample_size(data, labels, T = 2000, C = 8, N = 100, old_T = 5420):
+# reduce sample size from 5420 to 2000 to make covariance computations feasible
 
 
-    new_data = np.ones(((T*C),N))
-    new_NN_labels = np.ones(T*C)
+def reduce_sample_size(data, labels, T=2000, C=8, N=100, old_T=5420):
+
+    new_data = np.ones(((T * C), N))
+    new_NN_labels = np.ones(T * C)
 
     index = 0
     for x in range(C):
         for y in range(T):
-    
-            new_data[index] = data[x*old_T+y]
-            new_labels[index] = old_NN_labels[x*old_T+y]
-        
-            index +=1
+
+            new_data[index] = data[x * old_T + y]
+            new_labels[index] = old_NN_labels[x * old_T + y]
+
+            index += 1
 
     return new_data, new_labels
 
@@ -37,15 +40,15 @@ def mean_center(data):
     N = data.shape[1]
     C = data.shape[2]
 
-    #reshape from ((T*C),N) to (T,C,N)
-    data_2D = np.reshape(data, (T,C,N), order = 'F')
+    # reshape from ((T*C),N) to (T,C,N)
+    data_2D = np.reshape(data, (T, C, N), order="F")
 
-    #reshape from (T,C,N) to (T,N,C)
-    data_2D = np.swapaxes(data_2D,1,2)
+    # reshape from (T,C,N) to (T,N,C)
+    data_2D = np.swapaxes(data_2D, 1, 2)
 
     mean = u.first_moment(data_2D)
 
-    mean_reshaped = np.reshape(mean, ((T*C),N), order = 'F')
+    mean_reshaped = np.reshape(mean, ((T * C), N), order="F")
 
     mean_centered = data_2D - mean_reshaped
 
@@ -58,7 +61,7 @@ def TME_sample(mean_centered, mean_reshaped, num_samples):
     N = mean_centered.shape[1]
     C = mean_centered.shape[2]
 
-    sigma_T, sigma_N, sigma_C = cov_3D(mean_centered, T,N,C)
+    sigma_T, sigma_N, sigma_C = cov_3D(mean_centered, T, N, C)
 
     sizes = (T, N, C)
     covs = [sigma_T, sigma_N, sigma_C]
@@ -70,23 +73,23 @@ def TME_sample(mean_centered, mean_reshaped, num_samples):
 
     surrogates_reshaped = []
 
-    #add back mean to surrogates
+    # add back mean to surrogates
     for surr in surrogates:
 
-        #reshape from (T,N,C) to ((T*C), N)
-        surrogate_dataset = np.reshape(surr,((T*C),N), order = 'F')
+        # reshape from (T,N,C) to ((T*C), N)
+        surrogate_dataset = np.reshape(surr, ((T * C), N), order="F")
 
         surrogate_dataset = surrogate_dataset + mean_reshaped
 
         surrogates_reshaped.append(surrogate_dataset)
-    
+
     return surrogates_reshaped
 
 
 if __name__ == "__main__":
 
-    import_randtensor('/Users/anastasia/Desktop/randtensor')
-    
+    import_randtensor("/Users/anastasia/Desktop/randtensor")
+
     model = keras.models.load_model("./values/model1.h5")
 
     old_NN_data = np.load("./values/old_NN_data.npy")
@@ -98,9 +101,3 @@ if __name__ == "__main__":
     NN_mean_centered = mean_center(NN_data)
 
     surrogate_data = TME_sample(NN_mean_centered)
-
-    
-
-
-
-    
