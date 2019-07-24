e46012b0f5654500a1b7a2f563cd91a1af3414f7
Anastasia Dmitrienko <34189027+admitrienko@users.noreply.github.com>
2019-07-24 16:45:21 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='MNIST_Abstraction_Testing',
+      version='0.1',
+      description='Git template.',
+      author='Anastasia Dmitrienko',
+      author_email='ad3473@columbia.edu',
+      dependency_links=['https://github.com/gamaleldin/rand_tensor/tarball/master#egg=package-1.0'],
+      install_requires=['numpy', 
+                       'keras',
+                       'tensorflow',
+                        'matplotlib',
+                        'sklearn',
+                        'scipy'
+                       ],
+      packages = ['MNIST_Abstraction_Testing'],
+     )
