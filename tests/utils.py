6b3f9db0b7164d091bc2406b8b3024c78fcf5965
Anastasia Dmitrienko <34189027+admitrienko@users.noreply.github.com>
2019-08-02 10:50:07 -0400

    Update utils.py

--- tests/utils.py
+++ tests/utils.py
@@ -790,10 +790,9 @@ def error_bar_plot(
 ):
 
 
-    
 	if individual_sort:
-        
-		NN_means = np.sort(NN_means)
+		
+        	NN_means = np.sort(NN_means)
 		NN_means = np.flip(NN_means)
 		TME_means, TME_sd = sort(TME_means, TME_sd)
 		random_means, random_sd = sort(random_means, random_sd)
