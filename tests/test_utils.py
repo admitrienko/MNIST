fe8c2a9a8aaa06d142b173af42b158c5c2593b70
Anastasia Dmitrienko <34189027+admitrienko@users.noreply.github.com>
2019-08-02 13:57:28 -0400

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -19,8 +19,8 @@ from mpl_toolkits.mplot3d import Axes3D
 from scipy import stats
 from sklearn.decomposition import PCA
 
-import randtensor as r
-from randtensor import utils as u
+import rand_tensor as r
+#from rand_tensor import utils as u
 
 
 def test_cosine():
